8e9ce55b51aded6cc23bb2baa2b445b5daa0b96b
MountCD <mountcd@proton.me>
2026-08-01 11:09:26 +0700

    New config format

--- src/config.rs
+++ src/config.rs
@@ -1,59 +1,38 @@
-use std::error::Error;
 use std::fs::{read_to_string};
-use walkdir::{DirEntry, WalkDir};
 use crate::llm;
+use serde::{Serialize, Deserialize};
 
+#[derive(Debug, Deserialize)]
+struct Prompt {
+    system_prompt: String,
+    personal_prompt: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct TalkModel {
+    enabled: bool,
+    model_id: String,
+    api_url: String,
+}
+#[derive(Debug, Deserialize)]
+struct WorkerModel {
+    enabled: bool,
+    model_id: String,
+    api_url: String,
+}
+
+#[derive(Debug, Deserialize)]
+struct AudioModel {
+    enabled: bool,
+    model_id: String,
+    api_url: String,
+}
+
+#[derive(Debug, Deserialize)]
 struct Config {
-    system: String,
-    personality: String,
+    vault_path: String,
+    prompt: Prompt,
+    talk: TalkModel,
+    worker: WorkerModel,
+    audio: AudioModel,
 }
-impl Config {
-    pub fn new() -> Config {
-        Config {
-            system: "system".to_string(),
-            personality: "personality".to_string(),
-        }
-    }
-
-    fn push_system(&mut self, name: &str) {
-        self.system.push_str(name);
-    }
-    fn push_personality(&mut self, name: &str) {
-        self.personality.push_str(name);
-    }
-
-    fn export(&self) -> String {
-        let mut output: String = String::new();
-        output.push_str(&self.system);
-        output.push_str(&self.personality);
-        output
-    }
-}
-
-fn read_md(file: walkdir::Result<DirEntry>) -> Result<String, Box<dyn Error>> {
-    let file = file?;
-    let file = file.file_name();
-    let buffer = read_to_string(file)?;
-
-    Ok(buffer)
-}
-
-pub fn open_config() -> String {
-    let binding = std::env::current_dir().unwrap();
-    let _config_path: &str = binding.to_str().unwrap();
-    let mut config = Config::new();
-
-    for entry in WalkDir::new(&_config_path) {
-        if !entry.as_ref().unwrap().path().is_dir() {
-            continue;
-        }
-        if entry.as_ref().unwrap().path().ends_with("system.md") {
-            config.push_system(&*read_md(entry).unwrap());
-        } else if entry.as_ref().unwrap().path().ends_with("personality.md") {
-            config.push_personality(&*read_md(entry).unwrap());
-        }
-    }
-
-    config.export()
-}
-
