fafdfe1b47906f081c05b35e9abbf78c625a36b7
MountCD <mountcd@proton.me>
2026-08-04 16:27:43 +0700

    making a new structured metod od creating json requests

--- src/llm.rs
+++ src/llm.rs
@@ -1,12 +1,42 @@
 use crate::config;
 use anyhow::Result;
 use reqwest::Client;
+use serde::{Deserialize, Serialize};
 use serde_json::{Value, json};
 
+#[derive(Serialize, Debug)]
 pub enum Models {
     TalkModel,
     WorkerModel,
     AudioModel,
+}
+#[derive(Debug, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Roles {
+    System,
+    User,
+    Assistant,
+}
+#[derive(Debug, Serialize)]
+struct JsonRequest {
+    model: Models,
+    messages: Vec<JsonMessageContent>, // String -> struct
+    stream: bool,
+}
+impl JsonRequest {
+    pub fn new(model: Models, messages: Vec<JsonMessageContent>, stream: bool) -> Self {
+        JsonRequest { model, messages, stream }
+    }
+}
+#[derive(Debug, Serialize)]
+struct JsonMessageContent {
+    role: Roles,
+    message: String,
+}
+impl JsonMessageContent {
+    pub fn new(role: Roles, message: String) -> Self {
+        JsonMessageContent { role, message }
+    }
 }
 pub async fn make_request(client: &Client, model: Models, message: &str) -> Result<String> {
     let config = config::load_config();
@@ -46,13 +76,14 @@ pub async fn make_request(client: &Client, model: Models, message: &str) -> Resu
             }
         ]
     });
-    dbg!(&req);
 
     let mut req = client.post(api_url_model).json(&req);
     req = req.bearer_auth(api_key_model);
     dbg!(&req);
 
-    let response = req.send().await?.json::<Value>().await?;
+    let response = req.send().await?;
+    dbg!(&response);
+    let response = response.json::<Value>().await?;
     dbg!(&response);
 
     let content = response["choices"][0]["message"]["content"]
